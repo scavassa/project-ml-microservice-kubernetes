331dceb6ad1f4f673ce8d9c5bff7ea7b8779fdb7
scavassa <andre@scavassa.com>
2021-03-29 18:03:12 -0300

    Added kubernetes commands

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=scavassa/udacity-prediction
 
 # Step 2
-# Run the Docker Hub container with kubernetes
-
+# Run the Docker Hub container with kubernete
+kubectl run udacity-prediction --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity-prediction 8000:80
