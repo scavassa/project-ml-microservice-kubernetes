1ed4e5f4a9bcdeb54879f1cd234437a6b99f6e13
scavassa <andre@scavassa.com>
2021-03-29 16:32:45 -0300

    added upload instructions to docker registry

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=scavassa/udacity-prediction
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag udacity-prediction $dockerpath
+docker login
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
